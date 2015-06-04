1f869d834429c7724090be9d46bbf0c6565e3b25
Mikel Blake <mikel.blake@gmail.com>
2015-06-04 13:38:40 -0600

    finished

--- service.js
+++ service.js
@@ -1,11 +1,17 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
+  	var deferred = $q.defer();
     return $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response){
+    	console.log(response);
+    	var parsedResponse = response.data.data
+      deferred.resolve(parsedResponse)
+      return deferred.promise;
     })
   }
 
